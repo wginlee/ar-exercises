923c4de99fa5a04239061edc5a0ca026e3e772c6
Graham Lee <wgin.lee@gmail.com>
2016-12-06 23:22:57 +0000

    ex7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { greater_than: 10, less_than: 200}
+  validates :store, presence: true
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :apparel_present
+
+  def apparel_present
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:mens_apparel, 'or womens apparel (at least one) must be true')
+      errors.add(:womens_apparel, 'or mens apparel (at least one) must be true')
+    end
+  end
+
+
+
 end
