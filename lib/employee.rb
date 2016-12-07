d7226070a59919f05752f180a36b10a879419d38
Graham Lee <wgin.lee@gmail.com>
2016-12-07 00:31:09 +0000

    ex8

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,12 @@ class Employee < ActiveRecord::Base
   validates :hourly_rate, numericality: { greater_than: 10, less_than: 200}
   validates :store, presence: true
 
+  before_create :set_password
+
+  private
+    def set_password
+      self.password = [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+    end
+
+
 end
